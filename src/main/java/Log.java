0553972f60764378e76f8198a9b36638dbd0a8c4
Hashem <hashemabualteen@gmail.com>
2022-08-08 13:15:57 +0300

    changes timestamp from long to String

--- src/main/java/Log.java
+++ src/main/java/Log.java
@@ -1,11 +1,11 @@
 public class Log {
 
-    private long timestamp;
+    private String timestamp;
     private int clientId;
     private String description;
     private double amount;
 
-    public Log(long timestamp, int clientId, String description, double amount) {
+    public Log(String timestamp, int clientId, String description, double amount) {
         this.timestamp = timestamp;
         this.clientId = clientId;
         this.description = description;
