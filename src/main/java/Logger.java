512f9100b1acd686140f30baf82ac1d71bc30f51
Hashem <hashemabualteen@gmail.com>
2022-08-08 12:43:35 +0300

    Implements Logger

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -1,2 +1,25 @@
 public class Logger {
+    /*
+    * hold a DB driver for future use
+    */
+    private String driverName = "";
+
+    public Logger(String driverName) {
+        this.driverName = driverName;
+    }
+    /*
+    * accepts a Log object and stores it in the Log DB
+    */
+    public void log(Log log){
+        System.out.println(log.getData());
+    }
+
+    /*
+     *  empty for now
+     */
+    public Log[] getLogs(){
+        return null;
+    }
+
+
 }
